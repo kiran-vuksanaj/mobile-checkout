afd7e1455b361fe6dd56e63a26e7f84f72f102fe
Kiran Vuksanaj <kiranv@mit.edu>
2023-08-04 12:09:27 -0400

    new structure for startup

---
+++ sample.config.sh
@@ -0,0 +1,2 @@
+# COPY THIS FILE to `config.sh`, and insert the appropriate tokens.
+export SQUARE_ACCESS_TOKEN="{{Your Square Access Token}}";
---
+++ start.sh
@@ -0,0 +1,4 @@
+. venv/bin/activate
+. config.sh
+flask --app main.py run --host=0.0.0.0
+deactivate
